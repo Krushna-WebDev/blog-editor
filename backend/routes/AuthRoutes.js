6953b4773ccc225e6d37673b8b0bffd6fdb4e870
Krushna WebDev <krushna.webdev26@gmail.com>
2025-05-18 11:00:45 +0530

    changed

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controller/authController');
+const authController = require('../controller/AuthController');
 const authenticateToken = require('../middleware/authenticateToken');
 
 router.post('/signup', authController.signup);
